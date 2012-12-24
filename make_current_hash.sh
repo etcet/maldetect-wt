85fa4b2e0b7f8113e64a142c9afc713a16ca5be5
etcet <git@etcet.net>
2012-12-24 00:35:28 -0600

    Initial Commit * Added copyright line * Changed default quar_clean=0 * Changed default editor to vi * Fixed code formatting

---
+++ make_current_hash.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+inspath=./files
+currenthash=maldet-wt.current.hash
+md5sum $inspath/maldet $inspath/internals.conf $inspath/inotify/tlog $inspath/inotify/inotifywait $inspath/clean/* | awk '{print$1}' | tr '\n' ' ' | tr -d ' ' > $currenthash
+
+scp -P 2222 $currenthash chris@etcet.net:/home/chris/public_html/maldetect/
+
